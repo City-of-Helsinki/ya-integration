b7613b84856aae317083c517e3500d20b0adc8fc
marjutmikkola <marjut.mikkola@devikone.com>
2024-06-05 12:30:30 +0300

    starttiraha: Implement reading data from file, mapping and convert it to csv

---
+++ src/main/java/fi/hel/integration/ya/starttiraha/ControllerRouteBuilder.java
@@ -0,0 +1,52 @@
+package fi.hel.integration.ya.starttiraha;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jackson.JacksonDataFormat;
+
+import fi.hel.integration.ya.starttiraha.processor.Processor;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+
+@ApplicationScoped
+public class ControllerRouteBuilder extends RouteBuilder{
+
+    @Inject
+    Processor processor;
+    
+    @Override
+    public void configure() throws Exception {
+
+         // Exception handler for route errors. 
+        onException(Exception.class) // Catch all the Exception -type exceptions.
+            .log("An error occurred: ${exception}") // Log error.
+            .handled(true) // The error is not passed on to other error handlers.
+            .stop(); // Stop routing processing for this error.
+
+        from("direct:controller")
+            .setVariable("jsonData").simple("${body}")
+            .to("direct:controller.processPersonalData")
+            .to("file:outbox/starttiraha")
+            .setBody().variable("jsonData") // restore the original data body to route
+            .to("direct:controller.processPayrollTransaction")
+            .to("file:outbox/starttiraha") 
+        ;
+
+        // Henkilötietojen käsittely
+        from("direct:controller.processPersonalData")
+            .log("process body :: ${body}")
+            .unmarshal(new JacksonDataFormat())
+            .bean(processor, "createPersonalInfoMap")
+            .marshal().csv()
+            .setHeader(Exchange.FILE_NAME, simple("starttiraha_henkilotieto_testi_${date-with-timezone:now:Europe/Helsinki:yyyyMMddHHmmss}.csv"))
+        ;
+
+        // Palkkatapahtumien käsittely
+        from("direct:controller.processPayrollTransaction")
+            .unmarshal(new JacksonDataFormat())
+            .bean(processor, "createPayrollTransactionMap")
+            .marshal().csv()
+            .setHeader(Exchange.FILE_NAME, simple("starttiraha_palkkatapahtuma_testi_${date-with-timezone:now:Europe/Helsinki:yyyyMMddHHmmss}.csv"))
+        ;
+    }
+}
---
+++ src/main/java/fi/hel/integration/ya/starttiraha/processor/Processor.java
@@ -0,0 +1,222 @@
+package fi.hel.integration.ya.starttiraha.processor;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.camel.Exchange;
+import org.jboss.logging.Logger;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+
+@ApplicationScoped
+@Named ("processor")
+public class Processor {
+
+    @Inject
+    Logger log; 
+
+    private static final String EMPTY = "";
+    
+    @SuppressWarnings("unchecked")
+    public void createPersonalInfoMap(Exchange ex) {
+        try {
+            List<Map<String,Object>> body = ex.getIn().getBody(List.class);
+            List<Map<String,Object>> personalInfoMapList = new ArrayList<>();
+
+            for(Map<String,Object> item: body) {
+            
+                Map<String,Object> personalInfoMap = new LinkedHashMap<>();
+            
+                Map<String,Object> receiver = (Map<String, Object>) item.get("receiver");
+                String socialSecurityNo = (String) receiver.get("socialSecurityNumber");
+                String day = socialSecurityNo.substring(0, 2);
+                String month = socialSecurityNo.substring(2, 4);
+                String year = socialSecurityNo.substring(4, 6);
+                String mark = socialSecurityNo.substring(6, 7);
+            
+                // 1900-luvulla syntyneillä välimerkki sotussa on '-' tai 'Y'
+                // 2000-luvulla syntyneillä 'A'
+                if (mark.equals("-") || mark.equals("Y")) {
+                    year = "19" + year;
+            
+                } else {
+                    year = "20" + year;
+                }
+        
+                // muoto pp.kk.vvvv
+                String dateOfBirth = day + "." + month + "." + year;
+                System.out.println("dateOfBirth :: " + dateOfBirth);
+            
+                String personalId = socialSecurityNo.substring(6);
+                System.out.println("personalId :: " + personalId);
+            
+                Map<String,Object> name = (Map<String, Object>) receiver.get("name");
+                String lastName = (String) name.get("lastName");
+                String firstName = (String) name.get("firstName");
+            
+                Map<String,Object> postalAddress = (Map<String, Object>) receiver.get("postalAddress");
+                ArrayList<String> addressList = (ArrayList<String>) postalAddress.get("addressLine");
+                String address = addressList.get(0);
+            
+                String postalCode = (String) postalAddress.get("postalCode");
+                String postOffice = (String) postalAddress.get("postOffice");
+                String postalCodeAndOffice = postalCode + " " + postOffice;
+                System.out.println("postal code and office :: " + postalCodeAndOffice);
+            
+            
+                Map<String,Object> bankAccount = (Map<String, Object>) receiver.get("bankAccount");
+                String bankAccountNo = (String) bankAccount.get("value");
+                System.out.println("accountNo :: " + bankAccountNo);
+            
+                String periodStartDate = (String) item.get("periodStartDate");
+                String periodEndDate = (String) item.get("periodEndDate");
+
+                if(periodStartDate != null) {
+                    periodStartDate = convertDate(periodStartDate);
+            
+                } else {
+                    periodStartDate = null;
+                }
+
+                if(periodEndDate != null) {
+                    periodEndDate = convertDate(periodEndDate);
+            
+                } else {
+                    periodEndDate= null;
+                }
+            
+                personalInfoMap.put("id", EMPTY);
+                personalInfoMap.put("dateOfBirth", dateOfBirth);
+                personalInfoMap.put("personalId", personalId);
+                personalInfoMap.put("lastName", lastName);
+                personalInfoMap.put("firstName", firstName);
+                personalInfoMap.put("name1", EMPTY);
+                personalInfoMap.put("name2", EMPTY);
+                personalInfoMap.put("nativeLanguage", EMPTY);
+                personalInfoMap.put("language", EMPTY);
+                personalInfoMap.put("address", address);
+                personalInfoMap.put("postalCodeAndOffice", postalCodeAndOffice);
+                personalInfoMap.put("bankAccountNo", bankAccountNo);
+                personalInfoMap.put("bic1", EMPTY);
+                personalInfoMap.put("deliveryMethod", EMPTY);
+                personalInfoMap.put("periodStartDate", periodStartDate);
+                personalInfoMap.put("continuityDate", EMPTY);
+                personalInfoMap.put("task", EMPTY);
+                personalInfoMap.put("group", EMPTY);
+                personalInfoMap.put("leaveProcessing", EMPTY);
+                personalInfoMap.put("pensionSystem", EMPTY);
+                personalInfoMap.put("accidentInsuranceCompany", EMPTY);
+                personalInfoMap.put("insuranceException", EMPTY);
+                personalInfoMap.put("periodEndDate", periodEndDate);
+                personalInfoMap.put("terminationReason", EMPTY);
+                personalInfoMap.put("compantyId", EMPTY);
+                personalInfoMap.put("businessUnit", EMPTY);
+                personalInfoMap.put("costCenter", EMPTY);
+                personalInfoMap.put("profitCenter", EMPTY);
+                personalInfoMap.put("internalOrder", EMPTY);
+                personalInfoMap.put("project", EMPTY);
+                personalInfoMap.put("functionalArea", EMPTY);
+                personalInfoMap.put("supervisor", EMPTY);
+                personalInfoMap.put("paymentDueDate", EMPTY);
+                personalInfoMap.put("paymentType", EMPTY);
+                personalInfoMapList.add(personalInfoMap);
+            }
+
+            ex.getIn().setBody(personalInfoMapList);
+        
+        } catch (Exception e){
+            log.error(e);
+            e.printStackTrace();
+            ex.setException(e);
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public void createPayrollTransactionMap(Exchange ex) {
+        try {
+            List<Map<String,Object>> body = ex.getIn().getBody(List.class);
+            List<Map<String,Object>> payrollTransactionMapList = new ArrayList<>();
+
+            for(Map<String,Object> item: body) {
+                Map<String,Object> payrollTransactionMap = new LinkedHashMap<>();
+                
+                Map<String,Object> receiver = (Map<String, Object>) item.get("receiver");
+                String socialSecurityNo = (String) receiver.get("socialSecurityNumber");
+            
+                String paymentPeriodStartDate = (String) item.get("paymentPeriodStartDate");
+                String paymentPeriodEndDate = (String) item.get("paymentPeriodEndDate");
+                if(paymentPeriodStartDate != null) {
+                    paymentPeriodStartDate = convertDate(paymentPeriodStartDate);
+            
+                } else {
+                    paymentPeriodStartDate = null;
+                }
+
+                if(paymentPeriodEndDate != null) {
+                    paymentPeriodEndDate = convertDate(paymentPeriodEndDate);
+            
+                } else {
+                    paymentPeriodEndDate= null;
+                }
+                
+                double grossSum = (double) item.get("grossSum");
+                String grossSumString = String.valueOf(grossSum).replace('.', ',');
+                System.out.println("GrossSumString :: " + grossSumString);
+    
+                Map<String,Object> view = (Map<String, Object>) item.get("view");
+                String grantDecisionRecordNumber = (String) view.get("grantDecisionRecordNumber");
+
+                payrollTransactionMap.put("socialSecurityNo", socialSecurityNo);
+                payrollTransactionMap.put("paymentPeriodStartDate", paymentPeriodStartDate);
+                payrollTransactionMap.put("paymentPeriodEndDate", paymentPeriodEndDate);
+                payrollTransactionMap.put("payType", EMPTY);
+                payrollTransactionMap.put("grossSum", grossSumString);
+                payrollTransactionMap.put("compantyId", EMPTY);
+                payrollTransactionMap.put("businessUnit", EMPTY);
+                payrollTransactionMap.put("costCenter", EMPTY);
+                payrollTransactionMap.put("profitCenter", EMPTY);
+                payrollTransactionMap.put("internalOrder", EMPTY);
+                payrollTransactionMap.put("project", EMPTY);
+                payrollTransactionMap.put("functionalArea", EMPTY);
+                payrollTransactionMap.put("handler", EMPTY);
+                payrollTransactionMap.put("grantDecisionRecordNumber", grantDecisionRecordNumber);
+                payrollTransactionMapList.add(payrollTransactionMap);
+            }
+
+            ex.getIn().setBody(payrollTransactionMapList);
+
+        } catch (Exception e){
+            log.error(e);
+            e.printStackTrace();
+            ex.setException(e);
+        }
+    }
+
+    // Päivämäärän muoto csv:ssä pp.kk.vvvv
+    public String convertDate (String date) {
+        
+        SimpleDateFormat originalFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        SimpleDateFormat desiredFormat = new SimpleDateFormat("dd.MM.yyyy");
+        
+        try {
+            Date parsedDate = originalFormat.parse(date);
+            date = desiredFormat.format(parsedDate);
+            return date;
+            
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+    }
+}
