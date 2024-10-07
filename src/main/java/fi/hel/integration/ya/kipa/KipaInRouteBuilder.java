36245103691c181cc8207e6070454a49d32d031d
marjutmikkola <marjut.mikkola@devikone.com>
2024-10-07 16:22:25 +0300

    Enable fetching file names from kipa

--- src/main/java/fi/hel/integration/ya/kipa/KipaInRouteBuilder.java
+++ src/main/java/fi/hel/integration/ya/kipa/KipaInRouteBuilder.java
@@ -192,7 +192,7 @@ public class KipaInRouteBuilder extends RouteBuilder{
             .setHeader("username").simple("{{KIPA_SFTP_USER_P24}}")
             .setHeader("password").simple("{{KIPA_SFTP_PASSWORD_P24}}")
             .setHeader("directoryPath").simple("{{KIPA_DIRECTORY_PATH_P24}}")
-            //.to("direct:fetchFileNamesFromKipa")
+            .to("direct:fetchFileNamesFromKipa")
             //.to("direct:fetchDirectoriesFromKipa")
         ;
 
