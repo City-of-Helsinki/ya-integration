12873b6d4021515200582911ef8d795eee4d502c
marjutmikkola <marjut.mikkola@devikone.com>
2024-10-15 13:52:02 +0300

    Test sending report

--- src/main/java/fi/hel/integration/ya/kipa/KipaInRouteBuilder.java
+++ src/main/java/fi/hel/integration/ya/kipa/KipaInRouteBuilder.java
@@ -72,7 +72,7 @@ public class KipaInRouteBuilder extends RouteBuilder{
                 + "&strictHostKeyChecking=no"
                 + "&scheduler=quartz"         
                 + "&scheduler.cron={{MAKSULIIKENNE_QUARTZ_TIMER}}" 
-                + "&antInclude=YA_p24_091_20240820163853*"
+                + "&antInclude=YA_p24_091_20240823120252*"
             )   
             .routeId("kipa-P24") 
             .autoStartup("{{MAKSULIIKENNE_IN_AUTOSTARTUP}}")
